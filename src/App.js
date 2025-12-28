23320713818e17139e51fe5891ed2c1fa5cb1f75
krsnamdas <krsnamdas@gmail.com>
2025-12-29 00:32:11 +0400

    BigTicket Lottery Lucky Number Chooser Fun Project 	modified:   package-lock.json 	modified:   src/App.js 	modified:   src/ai.js 	modified:   src/smartGenerate.js

--- src/App.js
+++ src/App.js
@@ -2,40 +2,16 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
 import SmartAi from './smartGenerate';
-
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        JRP
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Social Media AI Kit by Keerat
+        <Typography variant="h4" component="h1" sx={{ mb: 2, textAlign: 'center' }}>
+          Bigticket Lottery Lucky Number Chooser
         </Typography>
-
-        <SmartAi></SmartAi>
-        <ProTip />
-        <Copyright />
+        <SmartAi />
       </Box>
     </Container>
   );
--- src/smartGenerate.js
+++ src/smartGenerate.js
@@ -1,49 +1,50 @@
 import * as React from 'react';
-import { TextField,Button,Typography } from '@mui/material';
+import { TextField, Button, Typography, Box } from '@mui/material';
 import { useState } from 'react';
 import { generateContent } from './ai';
 
-
 export default function SmartAi() {
+    const [userPrompt, setUserPrompt] = useState("");
+    const [aiGeneratedContent, setAiGeneratedContent] = useState("Your lucky numbers will appear here!");
 
-const [userPrompt,setUserPrompt] = useState("")
-const [aiGeneratedContent,setAiGeneratedContent] = useState("Nothing has been generated yet!")
-const [heading,setHeading] = useState("")
+    const generateAiMagic = () => {
+        setAiGeneratedContent("Consulting the stars for your numbers...");
+        generateContent(userPrompt).then(res => {
+            setAiGeneratedContent(res.choices[0].message.content);
+            setUserPrompt("");
+        }).catch(err => {
+            console.error(err);
+            setAiGeneratedContent("Oops! The crystal ball is foggy. Try again.");
+        });
+    }
 
+    return (
+        <Box sx={{ textAlign: 'center', p: 3 }}>
+            <Typography variant="h5" sx={{ mb: 2 }}>
+                Welcome to Lucky Number Chooser
+            </Typography>
+            <Typography sx={{ mb: 3, color: 'text.secondary' }}>
+                Tell the AI a bit about yourself or just ask for your Bigticket numbers!
+            </Typography>
 
-const generateAiMagic=()=>{
+            <TextField 
+                label="Ask for your lucky numbers..." 
+                variant="outlined" 
+                fullWidth 
+                onChange={(e) => setUserPrompt(e.target.value)} 
+                value={userPrompt}
+            />
+            <Button 
+                sx={{ mt: 2 }} 
+                variant="contained" 
+                onClick={generateAiMagic}
+            >
+                Generate Lucky Numbers
+            </Button>
 
-    setAiGeneratedContent("Generating content for you ...")
-    setHeading(userPrompt)
-    generateContent(userPrompt).then(res=>{
-        console.log(res)
-        setAiGeneratedContent(res.choices[0].message.content)
-        setUserPrompt("")
-    }).catch(err=>{
-        console.log(err)
-    })
-}
-
-return (
-    <>
-    <Typography sx={{ mt: 6, mb: 3, color: 'text.secondary' }}>
-        {'Use this Social Media AI Kit to generate blog posts, LinkedIn Posts or Instagram Captions. Make sure to specify what you want and provide the context accordingly.'}
-    </Typography>
-
-    <TextField id="standard-basic" label="Write your Prompt Here" variant="standard" sx={{ width: '300px', height: '50px' }} onChange={(e) => setUserPrompt(e.target.value)} />
-
-    <br />
-
-    <Button sx={{ marginTop: '8px' }} variant="contained" onClick={generateAiMagic}>Generate</Button>
-
-    <Typography sx={{ mt: 6, mb: 3, color: 'text.secondary' }}>
-        {'Ai Generated Content for your prompt '}  {heading!="" ? ": "+heading : ""}
-    </Typography>
-
-    <Typography sx={{ mt: 6, mb: 3, color: 'text.secondary' }}>
-        {aiGeneratedContent}
-    </Typography>
-
-    </>
-);
+            <Typography sx={{ mt: 4, fontWeight: 'bold' }}>
+                {aiGeneratedContent}
+            </Typography>
+        </Box>
+    );
 }
